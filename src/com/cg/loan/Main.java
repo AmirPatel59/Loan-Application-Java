357349c004784a89c17f248ec62c830d3c2a627d
Muppallasrihari <vtu7331@veltechuniv.edu.in>
2021-05-04 21:40:49 +0530

    Loan Application Service added

--- src/com/cg/loan/Main.java
+++ src/com/cg/loan/Main.java
@@ -1,7 +1,13 @@
 package com.cg.loan;
 
+import com.cg.loan.main.Menu;
+
 public class Main {
+	
 public static void main(String[] args) {
 	System.out.println("welcome to C-Evils Bank");
+	Menu start=new Menu();
+	
+	start.LoanApplicationMenu();
 }
 }
