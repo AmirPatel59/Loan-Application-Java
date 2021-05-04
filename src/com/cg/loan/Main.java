6e0e60d5cfec8a1979d9e812b77066115494085e
AMAGE <aniket-gangadhar.amage@capgemini.com>
2021-05-04 20:12:28 +0530

    Updated customer and login module

--- src/com/cg/loan/Main.java
+++ src/com/cg/loan/Main.java
@@ -1,7 +1,25 @@
 package com.cg.loan;
 
+import java.util.Scanner;
+
+import com.cg.loan.dto.LoanIssuer;
+import com.cg.loan.service.LoanIssuerService;
+
 public class Main {
 public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+
 	System.out.println("welcome to C-Evils Bank");
-}
+
+	System.out.println("Enter Email Id:");
+	String email=sc.next();
+	System.out.println("Enter Password:");
+	String password = sc.next();
+	
+	LoanIssuerService LIService = new LoanIssuerService();
+	LIService.login(email, password);
+	
+	}
+
 }
--- src/com/cg/loan/service/CustomerService.java
+++ src/com/cg/loan/service/CustomerService.java
@@ -2,31 +2,50 @@ package com.cg.loan.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import com.cg.loan.dto.Customer;
 
 public class CustomerService {
 	List<Customer> list = new ArrayList<Customer>();
+	Scanner sc = new Scanner(System.in);
 
-	public Customer addCustomer(Customer customer) {
+	public Customer addCustomer() {
+		System.out.println("Enter your Customer Id");
+		int id = sc.nextInt();
+		System.out.println("Enter  Customer First Name");
+		String firstName = sc.next();
+		System.out.println("Enter  Customer Last Name");
+		String lastName = sc.next();
+		System.out.println("Enter  Customer Password");
+		String password = sc.next();
+		System.out.println("Enter  Customer Email");
+		String email = sc.next();
+		System.out.println("Enter  Customer Gender");
+		String gender = sc.next();
+		System.out.println("Enter  Customer Age");
+		int age = sc.nextInt();
+		System.out.println("Enter  Customer Mobile Number");
+		long mobileNumber = sc.nextLong();
+		Customer customer=new Customer(id,firstName, lastName, password, email, gender, age, mobileNumber);
 		list.add(customer);
 		return customer;
 	}
 
 	public Customer viewCustomerById(int id) {
-		Customer customer = new Customer();
+//		Customer customer = new Customer();
 		
 		for (int i = 0; i < list.size(); i++) {
 			Customer customer1 = list.get(i);
 //			System.out.println(customer1);
-			if (customer1.getCustomerId() == id) {
-				customer = customer1;
-			} else {
-				return null;
-			}
+			if(customer1.getCustomerId() == id) {
+//				customer = customer1;
+				return customer1;
+			} 
+			
 		}
-
-		return customer;
+		return null;
+		
 	}
 
 	public List<Customer> viewAllCustomer() {
@@ -44,5 +63,32 @@ public class CustomerService {
 		}
 		
 		
+	}
+
+	public Customer updateCustomer(int id) { 
+		Customer customer = viewCustomerById(id);
+		if(customer == null) {
+			return null;
+		}
+		else {
+			int index = list.indexOf(customer);
+			System.out.println("Enter  Customer First Name");
+			String firstName = sc.next();
+			System.out.println("Enter  Customer Last Name");
+			String lastName = sc.next();
+			System.out.println("Enter  Customer Password");
+			String password = sc.next();
+			System.out.println("Enter  Customer Email");
+			String email = sc.next();
+			System.out.println("Enter  Customer Gender");
+			String gender = sc.next();
+			System.out.println("Enter  Customer Age");
+			int age = sc.nextInt();
+			System.out.println("Enter  Customer Mobile Number");
+			long mobileNumber = sc.nextLong();
+			Customer customer1=new Customer(id,firstName, lastName, password, email, gender, age, mobileNumber);
+			list.set(index, customer1);
+			return customer1;
+		}
 	}
 }
